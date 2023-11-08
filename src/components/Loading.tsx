947a6b9849bb1bb1ac68c175726306a33a0887cd
Lee-Junmo <108059303+ezurno@users.noreply.github.com>
2023-11-08 17:06:20 +0900

    SET: create loading animation

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,12 +1,14 @@
+import { dotPulse } from "ldrs";
+dotPulse.register();
+
 export default function Loading() {
   return (
     <div
-      className="fixed top-0 left-0 flex flex-col justify-around items-center w-screen h-screen
+      className="fixed top-0 left-0 flex flex-col justify-center gap-4 items-center w-screen h-screen
         bg-themePurple
     "
     >
-      <h1>ICON</h1>
-      <h2>loading</h2>
+      <l-dot-pulse size="43" speed="1.3" color="#EFC638" />
     </div>
   );
 }
