e341e3d19ebadbaecaf946f060993c29096da22b
Lee-Junmo <108059303+ezurno@users.noreply.github.com>
2023-11-13 17:39:21 +0900

    SET: singup api-connection

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,13 +1,16 @@
 import { useNavigate } from "react-router-dom";
 import TopBar from "../components/TopBar";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 interface ISignupProps {
-  userId: string;
-  nickname: string;
-  password: string;
-  passwordCheck: string;
+  memberEmail: string;
+  memberNm: string;
+  memberPwd: string;
+  memberPwdCheck: string;
 }
+
+interface ISendDataProps {}
 
 export default function Signup() {
   const nav = useNavigate();
@@ -18,7 +21,27 @@ export default function Signup() {
     setError,
   } = useForm<ISignupProps>();
 
-  const onValid = () => {};
+  const onValid = (data: ISignupProps) => {
+    console.log(data);
+    if (data.memberPwd !== data.memberPwdCheck) {
+      setError("memberPwdCheck", {
+        type: "server",
+        message: "비밀번호가 다릅니다.",
+      });
+    } else {
+      // 두 값이 같을 때
+      const url = "http://nineto6.kro.kr:8080/api/members/signup";
+
+      axios.post(url, data).then((response) => {
+        if (response.data.status === 201) {
+          console.log(response.data.message);
+          nav("/login");
+        } else {
+          console.log(response.data.message);
+        }
+      });
+    }
+  };
 
   return (
     <div className="flex flex-col w-full h-screen bg-themePurple">
@@ -32,50 +55,54 @@ export default function Signup() {
             className="flex flex-col w-full gap-4"
           >
             <input
-              {...register("userId", {
+              {...register("memberEmail", {
                 required: "아이디를 입력하세요.",
               })}
-              name="userId"
+              name="memberEmail"
               className="h-10 px-4"
               placeholder="아이디"
             />
             <input
-              {...register("nickname", {
+              {...register("memberNm", {
                 required: "닉네임을 입력하세요.",
               })}
-              name="nickname"
+              name="memberNm"
               className="h-10 px-4"
               placeholder="닉네임"
             />
             <input
-              {...register("password", {
+              {...register("memberPwd", {
                 required: "비밀번호를 입력하세요.",
               })}
-              name="password"
+              name="memberPwd"
               className="h-10 px-4"
               placeholder="비밀번호"
-              type="password"
+              type="memberPwd"
             />
 
             <input
-              {...register("passwordCheck", {
+              {...register("memberPwdCheck", {
                 required: "비밀번호 확인란을 입력하세요.",
               })}
-              name="passwordCheck"
+              name="memberPwdCheck"
               className="h-10 px-4"
               placeholder="비밀번호 확인"
-              type="password"
+              type="memberPwd"
             />
             <button className="bg-themeDarkPurple text-white h-10">
               회원 가입
             </button>
           </form>
           <h2 className="text-center mt-2 text-themeYellow">
-            {/* {errors.loginId
-              ? errors.loginId.message
-              : errors.loginPassword
-              ? errors.loginPassword.message
-              : ""} */}
+            {errors.memberEmail
+              ? errors.memberEmail.message
+              : errors.memberPwd
+              ? errors.memberPwd.message
+              : errors.memberNm
+              ? errors.memberNm.message
+              : errors.memberPwdCheck
+              ? errors.memberPwdCheck.message
+              : ""}
           </h2>
         </div>
         <div>
